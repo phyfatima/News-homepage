b7d44e6ad6777f1d9f6e4d5034250a0ba379520e
Fatima MOUMEN <phyatima@gmail.com>
2022-12-28 17:32:09 +0100

    Create the main home page

---
+++ assets/js/scripty.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('#openBtn').on('click',function(){
+        $('#mySidenav').addClass('active');
+            
+        });
+    $('#closeBtn').on('click',function(){
+            $('#mySidenav').removeClass('active');
+                
+    });
+});
---
+++ design/test.php
@@ -0,0 +1,3 @@
+<?php
+// Hook wordpress ? 
+ 
